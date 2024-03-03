194d22a0df3bb6a69d72e84a6069791e65bc652c
Gökhan Alkacir <alkacirg@gmail.com>
2024-03-03 16:10:49 +0100

    chores: minor cleanup of misplaced globals

--- lua/takedo/lazy/vimwiki.lua
+++ lua/takedo/lazy/vimwiki.lua
@@ -1,15 +1,3 @@
 return {
 	"vimwiki/vimwiki",
-	config = function()
-		vim.g.vimwiki_list = {
-			{ path = "~/vimwiki/", syntax = "markdown", ext = ".md" },
-			{ path = "~/vimwiki/Programming", syntax = "markdown", ext = ".md" },
-			{ path = "~/vimwiki/Linux", syntax = "markdown", ext = ".md" },
-			{ path = "~/vimwiki/DevOps", syntax = "markdown", ext = ".md" },
-		}
-		vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown", [".mdown"] = "markdown" }
-
-		--  Makes vimwiki markdown links as [text](text.md) instead of [text](text)
-		vim.g.vimwiki_markdown_link_ext = 1
-	end,
 }
